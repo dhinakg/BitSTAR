390b1460187d3ef885cff3d962a4f41b321fb1ee
Dhinak G <dhinak2004@gmail.com>
2018-01-29 18:23:05 -0500

    Update botutils.py

--- plugins/botutils.py
+++ plugins/botutils.py
@@ -328,7 +328,7 @@ async def onCommand(message_in):
     
     if message_in.command == 'restart':
         print("Rebooting...")
-        abcdeeee = new Message(body='PONG! Bot is up!')
+        abcdeeee = message.Message(body='PONG! Bot is up!')
         os._exit(1)
         return message.Message(body='Rebooting...')
 
