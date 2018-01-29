610937070a8688b04831136e88c7f8328ce09e87
Dhinak G <dhinak2004@gmail.com>
2018-01-29 18:22:11 -0500

    Invalid syntax?

--- plugins/botutils.py
+++ plugins/botutils.py
@@ -328,7 +328,7 @@ async def onCommand(message_in):
     
     if message_in.command == 'restart':
         print("Rebooting...")
-        Message x = new message.Message(body='PONG! Bot is up!')
+        message abcdeeee = new message.Message(body='PONG! Bot is up!')
         os._exit(1)
         return message.Message(body='Rebooting...')
 
